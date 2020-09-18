0459f824a4012860e6c3e5bcd7249f2af1777983
Raymondo <raymondo@raymondos-mbp.home>
2020-09-18 14:15:52 +0100

    initial commit

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <div>
+      <h3>routes</h3>
+    </div>
+  );
+};
+
+export default Routes;
